55df32880a0bd13193c02368125c20d7d6a6c606
Sathiswarj <slsjkr@gmail.com>
2025-12-17 18:28:56 +0530

    proucts page added

--- src/screens/(Products)/Products.js
+++ src/screens/(Products)/Products.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
 export default function Products() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
   return (
-    <View>
-      <Text>Products</Text>
+    <>
+    <View className='p-4'>
+      <View className='bg-white p-6 rounded-lg shadow-lg'>
+          <TextInput 
+            className='border border-gray-300 rounded-lg p-3 mb-4'
+            placeholder='Search products...'
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+            autoCapitalize="none"
+            placeholderTextColor={"#6B7280"}
+
+          />
+          <View className='space-y-4'>
+            <View className='p-4 bg-gray-100 rounded-lg'> 
+              
+            </View>
+          
+            </View>
+        </View>
     </View>
+    </>
   )
 }
